4b5be27848249679ef5ce20ef4f42ae13937b865
Shreya Jain <jainshreya2005@gmail.com>
2025-07-21 22:45:45 +0530

    chnagesss

--- src/assets/Contact.jsx
+++ src/assets/Contact.jsx
@@ -9,20 +9,20 @@ export const ContactUs = () => {
 
         <div className="contact-content">
           <ul className="contact-list">
-            <li className="contact-item">
-              <span className="contact-label">Instagram</span>
+              <li className="contact-item">
+              <span className="contact-label">Our Website</span>
               <a 
-                href="https://www.instagram.com/thehimalayandevs712?igsh=aXpuMHBteGpxNG1w"
+                href="https://www.thehimalayandevs.com"
                 className="contact-link"
               >
                 <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuH7c5cLpGehi0b4iQk90fXUzC9p7Ebla13w&s"
+                  src="https://i.ibb.co/ynWFqg0r/Himalayan-Dev-logo.png"
                   alt="Instagram"
                   className="contact-icon"
                 />
               </a>
             </li>
-
+            
             <li className="contact-item">
               <span className="contact-label">Email</span>
               <a 
@@ -36,6 +36,64 @@ export const ContactUs = () => {
                 />
               </a>
             </li>
+
+            <li className="contact-item">
+              <span className="contact-label">Portfolio</span>
+              <a 
+                href="https://himalayandevsportfolio.netlify.app/"
+                className="contact-link"
+              >
+                <img
+                  src="https://seeklogo.com/images/P/portfolio-plus-banking-software-logo-8D1D8862B8-seeklogo.com.png"
+                  alt="Instagram"
+                  className="contact-icon"
+                />
+              </a>
+            </li>
+
+                 <li className="contact-item">
+              <span className="contact-label">GitHub</span>
+              <a 
+                href="https://github.com/thehimalyandevs712"
+                className="contact-link"
+              >
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJvu0wx3DcRTh3u_Vfm4FDa4hyC18FS6wFTQ&s"
+                  alt="Instagram"
+                  className="contact-icon"
+                />
+              </a>
+            </li>
+
+            
+                 <li className="contact-item">
+              <span className="contact-label">LinkedIn</span>
+              <a 
+                href="https://www.linkedin.com/in/the-himalayan-devs-264392376"
+                className="contact-link"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/960px-LinkedIn_logo_initials.png"
+                  alt="Instagram"
+                  className="contact-icon"
+                />
+              </a>
+            </li>
+
+             <li className="contact-item">
+              <span className="contact-label">Instagram</span>
+              <a 
+                href="https://www.instagram.com/thehimalayandevs712?igsh=aXpuMHBteGpxNG1w"
+                className="contact-link"
+              >
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuH7c5cLpGehi0b4iQk90fXUzC9p7Ebla13w&s"
+                  alt="Instagram"
+                  className="contact-icon"
+                />
+              </a>
+            </li>
+           
           </ul>
 
           <div className="contact-number">
