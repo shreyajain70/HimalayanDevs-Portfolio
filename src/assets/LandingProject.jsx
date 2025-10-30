8d12682c09e0fdf3d1007b05d1f3f4abe2ad4214
thehimalayandevs712 <thehimalayandevs712@gmail.com>
2025-10-30 22:11:25 +0530

    new project added

--- src/assets/LandingProject.jsx
+++ src/assets/LandingProject.jsx
@@ -11,19 +11,19 @@ export const LandingProjects = () => {
 
         <li className="landing-project-card">
           <img
-            src="https://i.ibb.co/twWhcnV6/Landing-1.png"
+            src="/BeautyPageui.png"
             className="Landing-project-image"
           />
           <p className="landing-project-desc">
-            I developed a fully responsive e-commerce website using React.js, showcasing my skills in frontend development and API integration. The website features a modern and intuitive user interface, allowing users to browse products seamlessly across multiple pages. Key Features: React Router: Implemented React Router to create a multi-page application.
+            This Beauty Parlour Landing Page is a visually elegant and user-friendly web design created to showcase a modern salon’s services with style and sophistication. Built with a focus on luxury aesthetics, smooth user experience, and responsive layout, the page reflects the brand’s promise of beauty, care, and confidence.
           </p>
           <a
-            href="https://products-e-commerce-wpxm.vercel.app/"
+            href="https://seemabeautyparlour.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
             className="landing-project-link"
           >
-            https://products-e-commerce-wpxm.vercel.app/
+            https://seemabeautyparlour.netlify.app/
           </a>
         </li>
 
@@ -65,21 +65,40 @@ export const LandingProjects = () => {
 
         <li className="landing-project-card">
           <img
-            src="https://i.ibb.co/23hgJ3rF/Landing-4.png"
+            src="/TeacherPortfolioui.png"
             className="Landing-project-image"
           />
           <p className="landing-project-desc">
-            A basic React project featuring a search bar and a dark/light mode toggle, implemented using hooks.
+            Dedicated, passionate, and creative — this teacher portfolio highlights a journey of inspiring young minds and fostering a love for learning. With a focus on innovative teaching methods, personalized student engagement, and continuous growth, this portfolio showcases lesson plans, achievements, certifications, and classroom projects that reflect a commitment to academic excellence.
           </p>
           <a
-            href="https://basic-reactpage.netlify.app/"
+            href="https://nikitathapaporfolio.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
             className="landing-project-link"
           >
-            https://basic-reactpage.netlify.app/
+            https://nikitathapaporfolio.netlify.app/
           </a>
         </li>
+
+        <li className="landing-project-card">
+          <img
+            src="/DigitalMenuui.png"
+            className="Landing-project-image"
+          />
+          <p className="landing-project-desc">
+          The Digital Menu is an interactive and user-friendly platform designed to replace traditional paper menus with a sleek digital experience. It allows customers to explore food categories, view detailed descriptions, prices, and images.
+          </p>
+          <a
+            href="https://kingskitchen-digitalmenu.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="landing-project-link"
+          >
+          https://kingskitchen-digitalmenu.netlify.app/
+          </a>
+        </li>
+
       </ul>
     </div>
   );
